5567b82919f9cc25c597c6459e173ecb5970bbb3
time-killer-games <samuelvenable@hotmail.com>
2021-02-22 04:51:28 -0500

    Add files via upload

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # panoview
 360 degree cylindrical panorama viewer - can be used graphically or from the command line
 
-![select your panorama](https://i.imgur.com/Rpl7jIs.png)
+![select your panorama](https://i.imgur.com/hCtqmWe.png)
 
-![view panoramas in 3D](https://i.imgur.com/pGZsEtN.png)
+![view panoramas in 3D](https://i.imgur.com/xzxav2u.png)
--- panoview.cpp
+++ panoview.cpp
@@ -29,13 +29,15 @@
 #include <climits>
 
 #include <string>
+#include <thread>
+#include <chrono>
 
 #include "dlgmodule.h"
 #include "lodepng.h"
 
+#include <SDL2/SDL.h>
 #include <sys/types.h>
 #include <unistd.h>
-
 #if defined(__APPLE__) && defined(__MACH__)
 #include <GLUT/glut.h>
 #include <libproc.h>
@@ -46,50 +48,9 @@
 #include <GL/glut.h>
 #endif
 
-float angleX = 0.0f, xr = 0.0f;
-void CreateCylinder() {
-  const double PI = 3.141592653589793;
-  double i, resolution  = 0.3141592653589793;
-  double height = 3.5;
-  double radius = 0.5;
-  glPushMatrix();
-  glRotatef(90 + xr, 0, 90 + 2, 0);
-  glTranslatef(0, -1.75, 0);
-  glBegin(GL_TRIANGLE_FAN);
-  glTexCoord2f(0.5, 0.5);
-  glVertex3f(0, height, 0);
-  for (i = 2 * PI; i >= 0; i -= resolution) {
-    glTexCoord2f(0.5f * cos(i) + 0.5f, 0.5f * sin(i) + 0.5f);
-    glVertex3f(radius * cos(i), height, radius * sin(i));
-  }
-  glTexCoord2f(0.5, 0.5);
-  glVertex3f(radius, height, 0);
-  glEnd();
-  glBegin(GL_TRIANGLE_FAN);
-  glTexCoord2f(0.5, 0.5);
-  glVertex3f(0, 0, 0);
-  for (i = 0; i <= 2 * PI; i += resolution) {
-    glTexCoord2f(0.5f * cos(i) + 0.5f, 0.5f * sin(i) + 0.5f);
-    glVertex3f(radius * cos(i), 0, radius * sin(i));
-  }
-  glEnd();
-  glBegin(GL_QUAD_STRIP);
-  for (i = 0; i <= 2 * PI; i += resolution) {
-    const float tc = (i / (float)(2 * PI));
-    glTexCoord2f(tc, 0.0);
-    glVertex3f(radius * cos(i), 0, radius * sin(i));
-    glTexCoord2f(tc, 1.0);
-    glVertex3f(radius * cos(i), height, radius * sin(i));
-  }
-  glTexCoord2f(0.0, 0.0);
-  glVertex3f(radius, 0, 0);
-  glTexCoord2f(0.0, 1.0);
-  glVertex3f(radius, height, 0);
-  glEnd();
-  glPopMatrix();
-}
+namespace {
 
-std::string exeParentDirectory() {
+std::string ExecutableParentDirectory() {
   std::string fname;
   #if defined(__APPLE__) && defined(__MACH__)
   char exe[PROC_PIDPATHINFO_MAXSIZE];
@@ -116,46 +77,127 @@ std::string exeParentDirectory() {
     }
   }
   #endif
-  size_t fpos = fname.find_last_of("/");
+  size_t fpos = fname.find_last_of("/\\");
   return fname.substr(0, fpos + 1);
 }
 
-GLuint tex;
-int window = 0;
-void CreateTexture(const char *fname) {
+void LoadImage(unsigned char **out, unsigned *pngwidth, unsigned *pngheight, const char *fname) {
   unsigned char *data = nullptr;
-  unsigned pngwidth, pngheight;
-  unsigned error = lodepng_decode32_file(&data, &pngwidth, &pngheight, fname);
-  if (error) { glutDestroyWindow(window); exit(1); }
-  const int size = pngwidth * pngheight * 4;
+  unsigned error = lodepng_decode32_file(&data, pngwidth, pngheight, fname);
+  if (error) { return; } unsigned width = *pngwidth, height = *pngheight;
+
+  const int size = width * height * 4;
   unsigned char *buffer = new unsigned char[size]();
-  for (unsigned i = 0; i < pngheight; i++) {
-    for (unsigned j = 0; j < pngwidth; j++) {
-      unsigned oldPos = (pngheight - i - 1) * (pngwidth * 4) + 4 * j;
-      unsigned newPos = i * (pngwidth * 4) + 4 * j;
+
+  for (unsigned i = 0; i < height; i++) {
+    for (unsigned j = 0; j < width; j++) {
+      unsigned oldPos = (height - i - 1) * (width * 4) + 4 * j;
+      unsigned newPos = i * (width * 4) + 4 * j;
       buffer[newPos + 0] = data[oldPos + 0];
       buffer[newPos + 1] = data[oldPos + 1];
       buffer[newPos + 2] = data[oldPos + 2];
       buffer[newPos + 3] = data[oldPos + 3];
     }
   }
+
+  *out = buffer;
+  delete[] data;
+}
+
+GLuint tex;
+void LoadPanorama(const char *fname) {
+  unsigned char *data = nullptr;
+  unsigned pngwidth, pngheight;
+  LoadImage(&data, &pngwidth, &pngheight, fname);
+
   glGenTextures(1, &tex);
   glBindTexture(GL_TEXTURE_2D, tex);
+
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-  glTexImage2D(GL_TEXTURE_2D, 0,GL_RGBA, pngwidth, pngheight, 0, GL_RGBA, GL_UNSIGNED_BYTE, buffer);
-  delete[] buffer;
+
+  glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, pngwidth, pngheight, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
   delete[] data;
 }
 
+float xangle = 0.0f;
+float yangle = 0.0f;
+void DrawPanorama() {
+  const double PI = 3.141592653589793;
+  double i, resolution  = 0.3141592653589793;
+  double height = 3.5, radius = 0.5;
+
+  glPushMatrix(); glRotatef(90 + xangle, 0, 90 + 0.5, 0);
+  glTranslatef(0, -1.75, 0); glBegin(GL_TRIANGLE_FAN);
+
+  glTexCoord2f(0.5, 1); glVertex3f(0, height, 0);
+  for (i = 2 * PI; i >= 0; i -= resolution) {
+    glTexCoord2f(0.5f * cos(i) + 0.5f, 0.5f * sin(i) + 0.5f);
+    glVertex3f(radius * cos(i), height, radius * sin(i));
+  }
+
+  glTexCoord2f(0.5, 0.5); glVertex3f(radius, height, 0);
+  glEnd(); glBegin(GL_TRIANGLE_FAN);
+  glTexCoord2f(0.5, 0.5); glVertex3f(0, 0, 0);
+  for (i = 0; i <= 2 * PI; i += resolution) {
+    glTexCoord2f(0.5f * cos(i) + 0.5f, 0.5f * sin(i) + 0.5f);
+    glVertex3f(radius * cos(i), 0, radius * sin(i));
+  }
+
+  glEnd(); glBegin(GL_QUAD_STRIP);
+  for (i = 0; i <= 2 * PI; i += resolution) {
+    const float tc = (i / (float)(2 * PI));
+    glTexCoord2f(tc, 0.0);
+    glVertex3f(radius * cos(i), 0, radius * sin(i));
+    glTexCoord2f(tc, 1.0);
+    glVertex3f(radius * cos(i), height, radius * sin(i));
+  }
+
+  glTexCoord2f(0.0, 0.0);
+  glVertex3f(radius, 0, 0);
+  glTexCoord2f(0.0, 1.0);
+  glVertex3f(radius, height, 0);
+  glEnd(); glPopMatrix();
+}
+
+GLuint cur;
+void LoadCursor(const char *fname) {
+  unsigned char *data = nullptr;
+  unsigned pngwidth, pngheight;
+  LoadImage(&data, &pngwidth, &pngheight, fname);
+
+  glGenTextures(1, &cur);
+  glBindTexture(GL_TEXTURE_2D, cur);
+
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+
+  glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, pngwidth, pngheight, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
+  delete[] data;
+}
+
+void DrawCursor(GLuint texid, int curx, int cury, int curwidth, int curheight) {
+  glBindTexture(GL_TEXTURE_2D, texid); glEnable(GL_TEXTURE_2D);
+  glColor4f(1, 1, 1, 1); glBegin(GL_QUADS);
+
+  glTexCoord2f(0, 1); glVertex2f(curx, cury);
+  glTexCoord2f(0, 0); glVertex2f(curx, cury + curheight);
+  glTexCoord2f(1, 0); glVertex2f(curx + curwidth, cury + curheight);
+  glTexCoord2f(1, 1); glVertex2f(curx + curwidth, cury);
+  glEnd(); glDisable(GL_TEXTURE_2D);
+}
+
+SDL_Window *hidden = nullptr;
 void display() {
   glClearColor(0, 0, 0, 1);
   glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
   glMatrixMode(GL_PROJECTION);
   glLoadIdentity();
-  gluPerspective(90.0f, 16/9, 0.1f, 100.0);
+  gluPerspective(90.0f, 16 / 9, 0.1f, 100.0);
   glMatrixMode(GL_MODELVIEW);
   glLoadIdentity();
   glTranslatef(0, 0, 0);
@@ -164,24 +206,74 @@ void display() {
   glFrontFace(GL_CW);
   glEnable(GL_DEPTH_TEST);
   glLoadIdentity();
-  glRotatef(angleX, 2, 0, 0);
+  glRotatef(yangle, 1, 0, 0);
   glEnable(GL_TEXTURE_2D);
   glBindTexture(GL_TEXTURE_2D, tex);
-  CreateCylinder();
-  glFlush();
+  DrawPanorama(); glFlush();
+
+  SDL_Rect rect; if (hidden == nullptr) { return; }
+  int dpy = SDL_GetWindowDisplayIndex(hidden);
+  int err = SDL_GetDisplayBounds(dpy, &rect);
+  if (err == 0) {
+    glClear(GL_DEPTH_BITS);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    glOrtho(0, rect.w, rect.h, 0, -1, 1);
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+    glDisable(GL_CULL_FACE);
+    glDisable(GL_DEPTH_TEST);
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+    glEnable(GL_TEXTURE_2D);
+    glBindTexture(GL_TEXTURE_2D, cur);
+    DrawCursor(cur, (rect.w / 2) - 16, (rect.h / 2) - 16, 32, 32);
+    glBlendFunc(GL_ONE, GL_ZERO);
+  }
+
   glutSwapBuffers();
 }
 
-void special(int key, int x, int y) {
-  switch (key) {
-    case GLUT_KEY_RIGHT: xr += 2.0f; break;
-    case GLUT_KEY_LEFT:  xr -= 2.0f; break;
-    case GLUT_KEY_UP:    angleX -= (angleX > -28) ? 2.0f : 0; break;
-    case GLUT_KEY_DOWN:  angleX += (angleX <  28) ? 2.0f : 0; break;
-  }
-  glutPostRedisplay();
+double PanoramaSetVertAngle() {
+  return xangle;
 }
 
+void PanoramaSetHorzAngle(double hangle) {
+  xangle -= hangle;
+}
+
+double PanoramaGetVertAngle() {
+  return yangle;
+}
+
+void PanoramaSetVertAngle(double vangle) {
+  yangle -= vangle;
+  yangle  = ((yangle >= -28) ? yangle : -28);
+  yangle  = ((yangle <=  28) ? yangle :  28);
+}
+
+void WarpMouse() {
+  int mx, my; SDL_Rect rect;
+  SDL_GetGlobalMouseState(&mx, &my);
+  if (hidden == nullptr) { return; }
+  int dpy = SDL_GetWindowDisplayIndex(hidden);
+  int err = SDL_GetDisplayBounds(dpy, &rect);
+  if (err == 0) {
+    int dx = rect.x, hdw = rect.x + (rect.w / 2);
+    int dy = rect.y, hdh = rect.y + (rect.h / 2);
+    SDL_WarpMouseGlobal(hdw, hdh);
+    PanoramaSetHorzAngle((hdw - mx) / 20);
+    PanoramaSetVertAngle((hdh - my) / 20);
+  }
+}
+
+void timer(int i) {
+  WarpMouse();
+  glutPostRedisplay();
+  glutTimerFunc(5, timer, 0);
+}
+
+int window = 0;
 void keyboard(unsigned char key, int x, int y) {
   switch (key) {
     case 27: glutDestroyWindow(window); exit(0); break;
@@ -189,33 +281,48 @@ void keyboard(unsigned char key, int x, int y) {
   glutPostRedisplay();
 }
 
+} // anonymous namespace
+
 int main(int argc, char **argv) {
   glutInit(&argc, argv);
   glutInitDisplayMode(GLUT_DOUBLE);
+
+  SDL_Init(SDL_INIT_VIDEO);
+  hidden = SDL_CreateWindow("hidden",
+  SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 0, 0, 0);
+  if (hidden != nullptr) { SDL_HideWindow(hidden); }
+  if (window != 0) glutDestroyWindow(window);
   window = glutCreateWindow("");
   glutDisplayFunc(display);
   glutHideWindow();
+
   glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
-  const char *fname;
-  if (argc == 1) {
-    dialog_module::widget_set_owner((char *)"-1");
-    std::string dir = exeParentDirectory() + "examples";
-    chdir(dir.c_str()); dialog_module::widget_set_icon((char *)(dir + "/../icon.png").c_str());
-    fname = dialog_module::get_open_filename_ext((char *)"Portable Network Graphic (*.png)|*.png;*.PNG",
-      (char *)"burning_within.png", (char *)"", (char *)"Choose a 360 Degree Cylindrical Panoramic Image");
-    if (strcmp(fname, "") == 0) { glutDestroyWindow(window); exit(0); }
-  } else {
-    fname = argv[1];
-  }
+  std::string parpath = ExecutableParentDirectory();
+  std::string panorama; if (argc == 1) {
+  dialog_module::widget_set_owner((char *)"-1");
+  std::string dir = parpath + "examples";
+
+  chdir(dir.c_str()); dialog_module::widget_set_icon((char *)(parpath + "icon.png").c_str());
+  panorama = dialog_module::get_open_filename_ext((char *)"Portable Network Graphic (*.png)|*.png;*.PNG",
+  (char *)"burning_within.png", (char *)"", (char *)"Choose a 360 Degree Cylindrical Panoramic Image");
+  if (strcmp(panorama.c_str(), "") == 0) { glutDestroyWindow(window); exit(0); } } else { panorama = argv[1]; }
+  std::string defcursor = (argc > 2) ? argv[2] : parpath + "defcursor.png";
+
   glClearDepth(1.0f);
   glEnable(GL_DEPTH_TEST);
   glDepthFunc(GL_LEQUAL);
   glShadeModel(GL_SMOOTH);
   glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);
-  CreateTexture(fname);
-  glutSetCursor(GLUT_CURSOR_CROSSHAIR);
+
+  LoadPanorama(panorama.c_str());
+  LoadCursor(defcursor.c_str());
+
+  glutSetCursor(GLUT_CURSOR_NONE);
   glutKeyboardFunc(keyboard);
-  glutSpecialFunc(special);
+  glutTimerFunc(0, timer, 0);
+
+  WarpMouse();
+  std::this_thread::sleep_for (std::chrono::milliseconds(5));
   glutShowWindow();
   glutFullScreen();
   glutMainLoop();
