fb5d2e13096fb478872637ae441b0eed4d3588e2
time-killer-games <samuelvenable@hotmail.com>
2021-02-22 14:55:39 -0500

    Add files via upload

---
+++ build32.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+cd "${0%/*}"
+
+if [ $(uname) = "Darwin" ]; then
+  clang++ panoview.cpp Universal/lodepng.cpp macOS/dlgmodule.cpp macOS/config.cpp -o panoview -std=c++17 -lSDL2 -framework OpenGL -framework GLUT -framework Cocoa -m32
+elif [ $(uname) = "Linux" ]; then
+  g++ panoview.cpp Universal/lodepng.cpp xlib/dlgmodule.cpp -o panoview -std=c++17 -static-libgcc -static-libstdc++ -lSDL2 -lGL -lGLU -lglut -lm -lpthread -lX11 -lprocps -m32
+elif [ $(uname) = "FreeBSD" ]; then
+  clang++ panoview.cpp Universal/lodepng.cpp xlib/dlgmodule.cpp -o panoview -std=c++17 -lSDL2 -lGL -lGLU -lglut -lm -lpthread -lX11 -lutil -lc -lm -m32
+else
+  g++ panoview.cpp Universal/lodepng.cpp Win32/dlgmodule.cpp -o panoview.exe -std=c++17 -static -static-libgcc -static-libstdc++ -lSDL2 -lGL -lGLU -lglut -lm -m32
+fi
