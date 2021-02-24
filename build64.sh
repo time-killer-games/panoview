95c9078ea2190f06c85ece22a706a4c25f0bc3e4
time-killer-games <samuelvenable@hotmail.com>
2021-02-24 13:17:49 -0500

    Update build64.sh

--- build64.sh
+++ build64.sh
@@ -6,7 +6,7 @@ if [ $(uname) = "Darwin" ]; then
 elif [ $(uname) = "Linux" ]; then
   g++ panoview.cpp Universal/lodepng.cpp xlib/dlgmodule.cpp -o panoview -std=c++17 -static-libgcc -static-libstdc++ -lSDL2 -lGL -lGLU -lglut -lm -lpthread -lX11 -lprocps -m64
 elif [ $(uname) = "FreeBSD" ]; then
-  clang++ panoview.cpp Universal/lodepng.cpp xlib/dlgmodule.cpp -o panoview -std=c++17 -lSDL2 -lGL -lGLU -lglut -lm -lpthread -lX11 -lutil -lc -lm -m64
+  clang++ panoview.cpp Universal/lodepng.cpp xlib/dlgmodule.cpp -o panoview -std=c++17 -lSDL2 -lGL -lGLU -lglut -lm -lpthread -lX11 -lutil -lc -m64
 else
   g++ panoview.cpp Universal/lodepng.cpp Win32/dlgmodule.cpp -DFREEGLUT_STATIC -o panoview.exe -std=c++17 -static -L/c/msys64/mingw64/lib -static-libgcc -static-libstdc++ -lmingw32 -lSDL2main -lSDL2 -lfreeglut_static -lglu32 -lopengl32 -lm -lwinpthread -lgdiplus -lshlwapi -lcomctl32 -lole32 -loleaut32 -ldinput8 -ldxguid -ldxerr8 -luser32 -lgdi32 -lwinmm -limm32 -lsetupapi -lversion -luuid -mwindows -Wl,--subsystem,windows -m64
 fi
