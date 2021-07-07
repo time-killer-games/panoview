84f399e8edbcdba1a05fbac7c6f9c93af240fb1a
time-killer-games <samuelvenable@hotmail.com>
2021-07-07 18:14:29 -0500

    Update buildES.sh

--- buildES.sh
+++ buildES.sh
@@ -5,4 +5,6 @@ if [ $(uname) = "Linux" ]; then
   g++ panoview.cpp Universal/crossprocess.cpp Unix/lodepng.cpp xlib/dlgmodule.cpp -DFREEGLUT_GLES=ON -o panoview -std=c++17 -static-libgcc -static-libstdc++ -lSDL2 -lGL -lGLU -lglut -lm -lpthread -lX11 -lprocps
 elif [ $(uname) = "FreeBSD" ]; then
   clang++ panoview.cpp Universal/crossprocess.cpp Unix/lodepng.cpp xlib/dlgmodule.cpp -DFREEGLUT_GLES=ON -o panoview -std=c++17 -I/usr/local/include -L/usr/local/lib -lSDL2 -lGL -lGLU -lglut -lm -lpthread -lX11 -lprocstat -lutil -lc
+elif [ $(uname) = "DragonFly" ]; then
+  g++ panoview.cpp Universal/crossprocess.cpp Unix/lodepng.cpp xlib/dlgmodule.cpp -o -DFREEGLUT_GLES=ON panoview -std=c++17 -I/usr/local/include -L/usr/local/lib -lSDL2 -lGL -lGLU -lglut -lm -lpthread -lX11 -lkvm -lutil -lc
 fi
