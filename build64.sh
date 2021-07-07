775bcc60e277356c2f0754bc975bd5b8ec5acd8d
time-killer-games <samuelvenable@hotmail.com>
2021-07-07 18:13:43 -0500

    Update build64.sh

--- build64.sh
+++ build64.sh
@@ -7,6 +7,8 @@ elif [ $(uname) = "Linux" ]; then
   g++ panoview.cpp Universal/crossprocess.cpp Unix/lodepng.cpp xlib/dlgmodule.cpp -o panoview -std=c++17 -static-libgcc -static-libstdc++ -lSDL2 -lGL -lGLU -lglut -lm -lpthread -lX11 -lprocps -m64
 elif [ $(uname) = "FreeBSD" ]; then
   clang++ panoview.cpp Universal/crossprocess.cpp Unix/lodepng.cpp xlib/dlgmodule.cpp -o panoview -std=c++17 -I/usr/local/include -L/usr/local/lib -lSDL2 -lGL -lGLU -lglut -lm -lpthread -lX11 -lprocstat -lutil -lc -m64
+elif [ $(uname) = "DragonFly" ]; then
+  g++ panoview.cpp Universal/crossprocess.cpp Unix/lodepng.cpp xlib/dlgmodule.cpp -o panoview -std=c++17 -I/usr/local/include -L/usr/local/lib -lSDL2 -lGL -lGLU -lglut -lm -lpthread -lX11 -lkvm -lutil -lc -m64
 else
   cd "Universal"
   "/c/msys64/msys2_shell.cmd" -defterm -mingw32 -no-start -here -lc "g++ crossprocess.cpp -o crossprocess32.exe -std=c++17 -static-libgcc -static-libstdc++ -static -m32";
