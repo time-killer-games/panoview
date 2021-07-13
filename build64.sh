c3b872b288c45cb6ea94e916a41f8ba32db20675
time-killer-games <samuelvenable@hotmail.com>
2021-07-13 07:22:06 -0400

    Add files via upload

--- build64.sh
+++ build64.sh
@@ -10,5 +10,7 @@ elif [ $(uname) = "FreeBSD" ]; then
 elif [ $(uname) = "DragonFly" ]; then
   g++ panoview.cpp Universal/crossprocess.cpp Unix/lodepng.cpp xlib/dlgmodule.cpp -o panoview -std=c++17 -I/usr/local/include -L/usr/local/lib -lGL -lGLU -lglut -lm -lpthread -lX11 -lXrandr -lXinerama -lkvm -lutil -lc -m64
 else
-  g++ panoview.cpp Universal/crossprocess.cpp Win32/libpng-util.cpp Win32/dlgmodule.cpp /c/msys64/mingw64/lib/libpng.a /c/msys64/mingw64/lib/libz.a /c/msys64/mingw64/lib/libfreeglut_static.a -DXPROCESS_WIN32EXE_INCLUDES -DFREEGLUT_STATIC -o panoview.exe -std=c++17 -static -I/c/msys64/mingw64/inlcude -L/c/msys64/mingw64/lib -static-libgcc -static-libstdc++ -lmingw32 -lglu32 -lopengl32 -lgdiplus -lgdi32 -lshlwapi -lcomctl32 -lcomdlg32 -lole32 -lwinmm -Wl,--subsystem,windows -fPIC -m64
+  windres icon.rc -O coff -o icon.res
+  g++ panoview.cpp Universal/crossprocess.cpp Win32/libpng-util.cpp Win32/dlgmodule.cpp /c/msys64/mingw64/lib/libpng.a /c/msys64/mingw64/lib/libz.a /c/msys64/mingw64/lib/libfreeglut_static.a icon.res -DXPROCESS_WIN32EXE_INCLUDES -DFREEGLUT_STATIC -o panoview.exe -std=c++17 -static -I/c/msys64/mingw64/inlcude -L/c/msys64/mingw64/lib -static-libgcc -static-libstdc++ -lmingw32 -lglu32 -lopengl32 -lgdiplus -lgdi32 -lshlwapi -lcomctl32 -lcomdlg32 -lole32 -lwinmm -Wl,--subsystem,windows -fPIC -m64
+  rm -f icon.res
 fi
